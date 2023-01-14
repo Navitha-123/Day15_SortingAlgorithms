483637cf3ba8bf0fd4731cc26d1510ecba310f8a
navitha <navithareddy112@gmail.com>
2023-01-14 20:27:18 +0530

    [navitha] UC3_InsertionSort

---
+++ Day15_SortingAlgorithms/UC3_InsertionSort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15_SortingAlgorithms
+{
+    public class UC3_InsertionSort
+    {
+        public static void DisplayInsertion()
+        {
+            int[] array = new int[5] { 23, 9, 85, 12, 99 };
+            int n = 5, i, j, val, flag;
+            Console.WriteLine("insertion array");
+            Console.WriteLine("Intial array");
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            for (i = 1; i < n; i++)
+            {
+                val = array[1];
+                flag = 0;
+                for (j = i - 1; j >= 0 && flag != 1;)
+                {
+                    if (val < array[j])
+                    {
+                        array[j + 1] = array[j];
+                        j--;
+                        array[j + 1] = val;
+                    }
+                    else
+                        flag = 1;
+                }
+
+                Console.WriteLine("sorrted array:");
+                for (i = 0; i < n; i++)
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+        }
+    }
+}
