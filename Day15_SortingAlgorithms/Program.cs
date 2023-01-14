4e55136d95b53a7818f3f46037d68b056e52a026
navitha <navithareddy112@gmail.com>
2023-01-14 20:22:04 +0530

    [navitha] UC2_BinarysearchTree

--- Day15_SortingAlgorithms/Program.cs
+++ Day15_SortingAlgorithms/Program.cs
@@ -4,9 +4,22 @@
     {
         static void Main(string[] args)
         {
-            string str = "ABC";
-                int n = str.Length;
-            UC1_StringPermutations.Permute(str, 0, n - 1);
+            //string str = "ABC";
+            //    int n = str.Length;
+            //UC1_StringPermutations.Permute(str, 0, n - 1);
+
+            UC2_BinarysearchTree bn = new UC2_BinarysearchTree();
+            bn.Add(30);
+            bn.Add(40);
+            bn.Add(50);
+            bn.Add(60);
+            bn.Add(80);
+            Console.WriteLine("After Preorder Result Will be: ");
+            bn.TravesrePreorder(bn.root);
+            Console.WriteLine("After Postorder Result Will be: ");
+            bn.TravesrePostorder(bn.root);
+            Console.WriteLine("After Inorder Result Will be: ");
+            bn.TravesreInorder(bn.root);
         }
     }
 }
